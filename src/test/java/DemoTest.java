a4133d5612048ef77c768f9fb852fe0da6fabd46
archa <archa@DESKTOP-PV8OE5S>
2022-09-12 12:49:23 +0530

    Commit and push

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test(groups= "smoke")
+	
+	public void demoTest()
+	{
+		System.out.println("Hello Test");
+	}
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class SampleTest {
+@Test(groups= "RegionalRegression")
+	
+	public void sampleTest()
+	{
+		System.out.println("Hello sample Test");
+	}
+@Test
+public void sampleDataTest()
+{
+	System.out.println("Hello Test data");
+}
+}
+
